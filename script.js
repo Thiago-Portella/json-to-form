32b7b7adb13d1a0abc976121e69c9c5321ff66ba
Thiago-Portella <thiagoportellaferreira@gmail.com>
2024-05-20 12:15:32 -0300

    removendo algumas funcionalidades do código

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ document.getElementById('generateForm').addEventListener('click', function() {
         const form = document.getElementById('jsonForm');
         form.innerHTML = '';
         generateFormFields(jsonObject, form);
-        addNewFieldButton(form, jsonObject, '');
     } catch (e) {
         alert('JSON inválido');
     }
@@ -18,175 +17,126 @@ document.getElementById('updateJson').addEventListener('click', function() {
 });
 
 function generateFormFields(jsonObject, parentElement, parentKey = '') {
-    Object.keys(jsonObject).forEach(key => {
-        const value = jsonObject[key];
-        const fieldContainer = createFieldContainer(key, parentKey);
+    for (const key in jsonObject) {
+        if (jsonObject.hasOwnProperty(key)) {
+            const value = jsonObject[key];
+            const fieldContainer = document.createElement('div');
+            const label = document.createElement('label');
+            const fieldId = parentKey ? `${parentKey}_${key}` : key;
+            label.setAttribute('for', fieldId);
+            label.textContent = key;
+            fieldContainer.appendChild(label);
 
-        if (typeof value === 'object' && !Array.isArray(value)) {
-            createExpandableSection(fieldContainer, key, value, parentKey);
-        } else if (Array.isArray(value)) {
-            createExpandableList(fieldContainer, key, value, parentKey);
-        } else {
-            createInputField(fieldContainer, key, value, parentKey);
+            if (typeof value === 'object' && !Array.isArray(value)) {
+                createToggleButton(fieldContainer, key, fieldId, value, parentElement);
+            } else if (Array.isArray(value)) {
+                createArrayFields(fieldContainer, key, fieldId, value);
+            } else {
+                createInputField(fieldContainer, fieldId, value, key);
+            }
+
+            parentElement.appendChild(fieldContainer);
         }
-
-        parentElement.appendChild(fieldContainer);
-    });
+    }
 }
 
-function createFieldContainer(key, parentKey) {
-    const fieldContainer = document.createElement('div');
-    const label = document.createElement('label');
-    const fieldId = parentKey ? `${parentKey}_${key}` : key;
-    label.setAttribute('for', fieldId);
-    label.textContent = key;
-    fieldContainer.appendChild(label);
-    return fieldContainer;
-}
-
-function createExpandableSection(fieldContainer, key, value, parentKey) {
-    const toggleButton = createToggleButton(key);
-    const nestedContainer = document.createElement('div');
-    nestedContainer.style.display = 'none';
-
+function createToggleButton(fieldContainer, key, fieldId, value, parentElement) {
+    const toggleButton = document.createElement('button');
+    toggleButton.textContent = 'Expandir';
+    toggleButton.setAttribute('type', 'button');
+    toggleButton.setAttribute('aria-label', `Expandir Objeto ${key}`);
     toggleButton.addEventListener('click', function() {
         const isExpanded = toggleButton.textContent === 'Recolher';
         toggleButton.textContent = isExpanded ? 'Expandir' : 'Recolher';
         toggleButton.setAttribute('aria-label', `${toggleButton.textContent} ${key}`);
         nestedContainer.style.display = isExpanded ? 'none' : 'block';
     });
-
     fieldContainer.appendChild(toggleButton);
-    generateFormFields(value, nestedContainer, `${parentKey}_${key}`);
-    fieldContainer.appendChild(nestedContainer);
-    addNewFieldButton(nestedContainer, value, `${parentKey}_${key}`);
-}
 
-function createExpandableList(fieldContainer, key, value, parentKey) {
-    const toggleButton = createToggleButton(key);
     const nestedContainer = document.createElement('div');
     nestedContainer.style.display = 'none';
+    generateFormFields(value, nestedContainer, fieldId);
+    fieldContainer.appendChild(nestedContainer);
+}
 
+function createArrayFields(fieldContainer, key, fieldId, value) {
+    const toggleButton = document.createElement('button');
+    toggleButton.textContent = 'Expandir';
+    toggleButton.setAttribute('type', 'button');
+    toggleButton.setAttribute('aria-label', `Expandir Lista ${key}`);
     toggleButton.addEventListener('click', function() {
         const isExpanded = toggleButton.textContent === 'Recolher';
         toggleButton.textContent = isExpanded ? 'Expandir' : 'Recolher';
         toggleButton.setAttribute('aria-label', `${toggleButton.textContent} Lista ${key}`);
         nestedContainer.style.display = isExpanded ? 'none' : 'block';
     });
-
     fieldContainer.appendChild(toggleButton);
+
+    const nestedContainer = document.createElement('div');
+    nestedContainer.style.display = 'none';
     value.forEach((item, index) => {
-        const arrayFieldId = `${parentKey}_${key}_${index}`;
         const arrayLabel = document.createElement('label');
+        const arrayFieldId = `${fieldId}_${index}`;
         arrayLabel.setAttribute('for', arrayFieldId);
         arrayLabel.textContent = `${key}[${index}]`;
         nestedContainer.appendChild(arrayLabel);
-
         if (typeof item === 'object') {
-            createExpandableSection(nestedContainer, `${key}[${index}]`, item, `${parentKey}_${key}`);
+            createNestedArrayFields(nestedContainer, key, index, item, arrayFieldId);
         } else {
-            createInputField(nestedContainer, `${key}[${index}]`, item, `${parentKey}_${key}`);
+            createInputField(nestedContainer, arrayFieldId, item, `${key}[${index}]`);
         }
     });
     fieldContainer.appendChild(nestedContainer);
-    addNewFieldButton(nestedContainer, value, `${parentKey}_${key}`);
 }
 
-function createInputField(fieldContainer, key, value, parentKey) {
-    const input = document.createElement('input');
-    const fieldId = parentKey ? `${parentKey}_${key}` : key;
-    input.id = fieldId;
+function createNestedArrayFields(nestedContainer, key, index, item, arrayFieldId) {
+    const itemContainer = document.createElement('div');
+    const itemToggleButton = document.createElement('button');
+    itemToggleButton.textContent = 'Expandir';
+    itemToggleButton.setAttribute('type', 'button');
+    itemToggleButton.setAttribute('aria-label', `Expandir Objeto ${key}[${index}]`);
+    itemToggleButton.addEventListener('click', function() {
+        const isExpanded = itemToggleButton.textContent === 'Recolher';
+        itemToggleButton.textContent = isExpanded ? 'Expandir' : 'Recolher';
+        itemToggleButton.setAttribute('aria-label', `${itemToggleButton.textContent} ${key}[${index}]`);
+        itemNestedContainer.style.display = isExpanded ? 'none' : 'block';
+    });
+    itemContainer.appendChild(itemToggleButton);
 
+    const itemNestedContainer = document.createElement('div');
+    itemNestedContainer.style.display = 'none';
+    generateFormFields(item, itemNestedContainer, arrayFieldId);
+    itemContainer.appendChild(itemNestedContainer);
+    nestedContainer.appendChild(itemContainer);
+}
+
+function createInputField(fieldContainer, fieldId, value, key) {
+    let input;
     if (typeof value === 'boolean') {
+        input = document.createElement('input');
         input.type = 'checkbox';
+        input.id = fieldId;
         input.checked = value;
         input.setAttribute('aria-label', `${key} ${value ? 'true' : 'false'}`);
         input.addEventListener('change', function() {
             input.setAttribute('aria-label', `${key} ${input.checked ? 'true' : 'false'}`);
         });
     } else if (typeof value === 'number') {
+        input = document.createElement('input');
         input.type = 'text';
+        input.id = fieldId;
         input.value = value;
         input.setAttribute('pattern', '[0-9]*\\.?[0-9]*');
         input.addEventListener('input', function() {
             input.value = input.value.replace(/[^0-9.]/g, '');
         });
     } else {
+        input = document.createElement('input');
         input.type = 'text';
+        input.id = fieldId;
         input.value = value;
     }
-
     fieldContainer.appendChild(input);
-}
-
-function createToggleButton(key) {
-    const toggleButton = document.createElement('button');
-    toggleButton.textContent = 'Expandir';
-    toggleButton.setAttribute('type', 'button');
-    toggleButton.setAttribute('aria-label', `Expandir ${key}`);
-    return toggleButton;
-}
-
-function addNewFieldButton(parentElement, jsonObject, parentKey = '') {
-    const addButton = document.createElement('button');
-    addButton.textContent = `Adicionar Campo em ${parentKey || 'root'}`;
-    addButton.setAttribute('type', 'button');
-    addButton.addEventListener('click', function() {
-        const newFieldKey = prompt('Digite o nome do novo campo:');
-        if (newFieldKey) {
-            const newFieldTypeContainer = document.createElement('div');
-            const newFieldTypeLabel = document.createElement('label');
-            newFieldTypeLabel.textContent = 'Escolha o tipo do novo campo:';
-            const newFieldTypeSelect = document.createElement('select');
-            const fieldTypeOptions = ['string', 'number', 'boolean', 'object', 'array'];
-            fieldTypeOptions.forEach(option => {
-                const optionElement = document.createElement('option');
-                optionElement.value = option;
-                optionElement.textContent = option;
-                newFieldTypeSelect.appendChild(optionElement);
-            });
-            newFieldTypeContainer.appendChild(newFieldTypeLabel);
-            newFieldTypeContainer.appendChild(newFieldTypeSelect);
-            parentElement.appendChild(newFieldTypeContainer);
-
-            const confirmButton = document.createElement('button');
-            confirmButton.textContent = 'Confirmar';
-            confirmButton.setAttribute('type', 'button');
-            confirmButton.addEventListener('click', function() {
-                const newFieldType = newFieldTypeSelect.value;
-                let newFieldValue;
-                switch (newFieldType) {
-                    case 'string':
-                        newFieldValue = '';
-                        break;
-                    case 'number':
-                        newFieldValue = 0;
-                        break;
-                    case 'boolean':
-                        newFieldValue = false;
-                        break;
-                    case 'object':
-                        newFieldValue = {};
-                        break;
-                    case 'array':
-                        newFieldValue = [];
-                        break;
-                    default:
-                        alert('Tipo inválido!');
-                        return;
-                }
-                jsonObject[newFieldKey] = newFieldValue;
-                const newFieldContainer = document.createElement('div');
-                generateFormFields({ [newFieldKey]: newFieldValue }, newFieldContainer, parentKey);
-                parentElement.appendChild(newFieldContainer);
-                parentElement.removeChild(newFieldTypeContainer);
-                parentElement.removeChild(confirmButton);
-                parentElement.appendChild(addButton); // Move the add button to the end
-            });
-            parentElement.appendChild(confirmButton);
-        }
-    });
-    parentElement.appendChild(addButton);
 }
 
 function updateJsonFromForm(form) {
